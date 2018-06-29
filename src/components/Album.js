43eae92a2b15e690ff3c237f2531bb42be36d6bc
Matt Roe <coachroe10@gmail.com>
2018-06-29 10:38:20 -0500

    Checkpoint 10 Styling Seek Bar Fix: Completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -161,8 +161,10 @@ class Album extends Component {
           <PlayerBar
              isPlaying={this.state.isPlaying}
              currentSong={this.state.currentSong}
-             currentTime={this.formatTime(this.audioElement.currentTime)}
-             duration={this.formatTime(this.audioElement.duration)}
+             currentTime={this.audioElement.currentTime}
+             duration={this.audioElement.duration}
+             currentFormatTime={this.formatTime(this.audioElement.currentTime)}
+             durationFormatTime={this.formatTime(this.audioElement.duration)}
              volume={this.state.volume}
              handleSongClick={() => this.handleSongClick(this.state.currentSong)}
              handlePrevClick={() => this.handlePrevClick()}
--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -16,6 +16,7 @@ class PlayerBar extends Component {
           </button>
         </section>
         <section id="time-control">
+          <div className="current-time">{this.props.currentFormatTime}</div>
           <input
             type="range"
             className="seek-bar"
@@ -25,8 +26,7 @@ class PlayerBar extends Component {
             step="0.01"
             onChange={this.props.handleTimeChange}
           />
-          <div className="current-time">{this.props.currentTime}</div> of
-          <div className="total-time">{this.props.duration}</div>
+          <div className="total-time">{this.props.durationFormatTime}</div>
         </section>
         <section id="volume-control">
           <div className="icon ion-volume-low"></div>
